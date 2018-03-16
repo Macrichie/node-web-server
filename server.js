b5135ec06abc08983c54900f6fdc0c14de81337d
Macrichie <makanjuolakunle@gmail.com>
2018-03-16 16:23:40 +0100

    Add project page

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about', (req, res)=> {
     });
 });
 
+app.get('/projects', (req, res)=> {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects',
+    });
+});
+
+
 app.get('/bad', (req, res)=> {
     res.send({
         errorMessage: 'The site you request is not available'
