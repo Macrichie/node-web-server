0ecfcd2c9f5a8547092e723f38966d65302fe57d
Macrichie <makanjuolakunle@gmail.com>
2018-03-16 13:39:05 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFile('server.log', `${log}\n`, (err) => {
+        if(err) {
+            console.log('Unable to append to server log');
+        }
+    })
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my page'
+    });
+});
+
+app.get('/about', (req, res)=> {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req, res)=> {
+    res.send({
+        errorMessage: 'The site you request is not available'
+    });
+});
+
+app.listen(3000, ()=>{
+    console.log('server running on port 3000');
+});
